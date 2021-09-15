c83e6778b66e6b3ebe991c5ec7dd1f82494a75b2
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-15 06:05:15 +0300

    path for index

--- gram/urls.py
+++ gram/urls.py
@@ -3,14 +3,5 @@ from . import views
 
 urlpatterns=[
     path('', views.loginUser, name="index"),
-    path('home/', views.home, name="home"),   
-    path('register/', views.registerUser, name="registeruser"),
-    path('uploadimage/', views.new_image, name="uploadimage"),
-    path('viewimage/<int:pk>/', views.viewPhoto, name="viewphoto"),
-    path('likes/<int:pk>/', views.likes, name="likes"),
-    path('userprofile/<int:pk>/', views.profile_view, name="userprofile"),
-    path("followers/<int:pk>/", views.followers, name="followers"),
-    path("editpage/<int:pk>/", views.editpage, name="editprofile"), 
-    path("searchprofile/", views.search_profile, name="search_profile"),
-    path('logout/', views.logoutUser, name="logout")
+   
 ]
