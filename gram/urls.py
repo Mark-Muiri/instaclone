cc68f402428e251e634d5c4945165da5b702883f
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-15 06:07:36 +0300

    path for upload page

--- gram/urls.py
+++ gram/urls.py
@@ -5,5 +5,5 @@ urlpatterns=[
     path('', views.loginUser, name="index"),
     path('home/', views.home, name="home"),
     path('register/', views.registerUser, name="registeruser"),
-     
+    path('uploadimage/', views.new_image, name="uploadimage"),
 ]
